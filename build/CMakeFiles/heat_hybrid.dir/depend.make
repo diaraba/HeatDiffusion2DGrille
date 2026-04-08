eeb7d02527802ae908fd15763b4ded40a2458f52
diaraba <diarabadiarra9@gmail.com>
2026-04-08 15:12:17 +0000

    Suppression du dossier build/ du code a pusher

--- build/CMakeFiles/heat_hybrid.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for heat_hybrid.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/heat_omp.dir/src/boundary.cpp.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/heat_omp.dir/src/boundary.cpp.o: \
- /home/ashborn/DevProjects/HPC/HeatDiffusion2DGrille/src/boundary.cpp \
- /usr/include/stdc-predef.h \
- /home/ashborn/DevProjects/HPC/HeatDiffusion2DGrille/include/boundary.h \
- /home/ashborn/DevProjects/HPC/HeatDiffusion2DGrille/include/grid.h
